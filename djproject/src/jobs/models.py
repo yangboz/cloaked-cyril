8711ec0c92a0a670163e44166cd82eda17bd0d40
unknown <yangboz@yzhou24.asiapacific.hpqcorp.net>
2013-04-25 10:08:26 +0800

    CHANGED:applied the Location and Job class definition at models.py

--- djproject/src/jobs/models.py
+++ djproject/src/jobs/models.py
@@ -10,3 +10,13 @@ class Location(models.Model):
     def __str__(self):
         if self.state:
             return "%s,%s,%s" %(self.city,self.state,self.country)
+        else:
+            return "%s,%s" % (self.city,self.country)
+class Job(models.Model):
+    pub_date = models.DateField()
+    job_title = models.CharField(maxlength=50)
+    job_description = models.TextField()
+    location = models.ForeignKey(Location)
+    
+    def __str__(self):
+        return "%s (%s)" %(self.job_title,self.location)
