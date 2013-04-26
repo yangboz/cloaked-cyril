a9c9d1d28803f5c7b56c1b78fd44598d7f436b6b
unknown <yangboz@yzhou24.asiapacific.hpqcorp.net>
2013-04-26 21:08:24 +0800

    FIXED:invalid template base name for job_detail.html;

--- djproject/src/djproject/jobs/views.py
+++ djproject/src/djproject/jobs/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
-from django.template import Context,loader
+#from django.template import Context,loader
 from models import Job
 
 from django.shortcuts import get_object_or_404,render_to_response
@@ -14,6 +14,6 @@ def index(request):
 #    return HttpResponse(t.render(c))
     return render_to_response('jobs/job_list.html',{'object_list':object_list})
 
-def detail(request,object_id):
-    job = get_object_or_404(Job,pk=object_id)
+def detail(request,job_id):
+    job = get_object_or_404(Job,pk=job_id)
     return render_to_response('jobs/job_detail.html',{'object':job})
